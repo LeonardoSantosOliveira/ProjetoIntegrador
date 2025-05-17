9447cebd38ed925ecd4bd3b1c65bf959bb1cddfa
Leonardo Santos <leoleosantos21@gmail.com>
2025-05-17 18:22:12 -0300

    criação token

---
+++ server/common/protecaoAcesso.js
@@ -0,0 +1,50 @@
+var jwt = require('jsonwebtoken');
+var SchemaObject = require('node-schema-object');
+
+var UsuarioAcessoToken = new SchemaObject({ tokenAcesso: string },
+    {
+        methods: {
+
+            geraTokenAcesso(dados) {
+                try {
+                    return jwt.sign({ 'Email': dados.email, 'IdEmpresa': dados.idEmpresa, 'Nome': dados.nome }, 'Token', { expiresIn: 86400000 });
+                } catch (error) {
+                    console.log(error);
+                    throw error;
+                }
+            },
+
+            verificaTokenAcesso(req, res, next) {
+                var headerTokenAcesso = req.headers["authorization"]
+
+                if (typeof headerTokenAcesso != 'undefined') {
+                    try {
+                        jwt.verify(headerTokenAcessom, 'Token');
+                        next();
+                    } catch (error) {
+                        res.send(401);
+                    }
+                } else {
+                    res.send(401);
+                }
+            },
+
+            retornaCodigoAcessoToken(valor, req) {
+                var headerTokenAcesso = req.headers["authorization"];
+                var decoded = jwt.decode(headerTokenAcesso, { complete: true });
+
+                if (valor === 'IdEmpresa') {
+                    return decoded.payload.IdEmpresa;
+                }
+                if (valor === 'Email') {
+                    return decoded.payload.email;
+                }
+                if (valor === 'Nome') {
+                    return decoded.payload.Nome;
+                }
+            }
+        }
+    }
+)
+
+module.exports = UsuarioAcessoToken;
